bf2036b995608e985f7d9621a8ac19943f58a9f9
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2024-09-23 01:34:26 -0300

    se agrego diseño resposive

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 const $btnFlecha = document.getElementById("flecha");
-const $linkAbouMe = document.getElementById("AbouMe");
-const $linkTrabajos = document.getElementById("Trabajos");
-const $linkContacto = document.getElementById("Contacto");
+const $linkAbouMe = document.getElementsByClassName("AbouMe");
+const $linkTrabajos = document.getElementsByClassName("Trabajos");
+const $linkContacto = document.getElementsByClassName("Contacto");
+
+const $stnSobreMi = document.getElementById("stnSobreMi").getBoundingClientRect();
+const $stnTrabajos = document.getElementById("stnTrabajos").getBoundingClientRect();
+const $stnContacto = document.getElementById("stnContacto").getBoundingClientRect();
+
+const $nav = document.getElementById("header-fixed");
 
 const $form = document.getElementById("form");
 const $btnMailto = document.getElementById("mailForm");
 
 window.addEventListener("scroll", (e) => {
     let y = document.documentElement.scrollTop;
+    let x = screen.width;
 
     if(y === 0){
         $btnFlecha.classList.add("hide");
@@ -17,6 +24,18 @@ window.addEventListener("scroll", (e) => {
         $btnFlecha.classList.remove("hide");
         $btnFlecha.classList.add("active");
     }
+
+    if(x > 900){
+        if(y < 675){
+            $nav.classList.add("top");
+        }else if(y >= 675){
+            $nav.classList.remove("top");
+        }
+    }
+    else{
+        $nav.classList.remove("top");
+    }
+
 });
 
 
@@ -28,37 +47,51 @@ $btnFlecha.addEventListener("click", (e)=>{
 });
 
 
-$linkAbouMe.addEventListener("click", ()=>{
-    let seccion = document.getElementById("stnSobreMi");
-    let position = seccion.getBoundingClientRect();
-    console.log(position);
-
+$linkAbouMe[0].addEventListener("click", ()=>{
     window.scrollTo({
         behavior: "smooth",
-        top: (position.top-75)
+        top: ($stnSobreMi.top-75)
+    });
+});
+
+$linkAbouMe[1].addEventListener("click", ()=>{
+    window.scrollTo({
+        behavior: "smooth",
+        top: ($stnSobreMi.top-75)
     });
 });
 
 
-$linkTrabajos.addEventListener("click", ()=>{
-    let seccion = document.getElementById("stnTrabajos");
-    let position = seccion.getBoundingClientRect();
-    console.log(position);
-
+$linkTrabajos[0].addEventListener("click", ()=>{
     window.scrollTo({
         behavior: "smooth",
-        top: (position.top-75)
+        top: ($stnTrabajos.top-75)
     });
 });
 
-$linkContacto.addEventListener("click", ()=>{
+$linkTrabajos[1].addEventListener("click", ()=>{
+
+    window.scrollTo({
+        behavior: "smooth",
+        top: ($stnTrabajos.top-75)
+    });
+});
+
+$linkContacto[0].addEventListener("click", ()=>{
     let seccion = document.getElementById("stnContacto");
     let position = seccion.getBoundingClientRect();
     console.log(position);
 
     window.scrollTo({
         behavior: "smooth",
-        top: (position.top-75)
+        top: ($stnContacto.top-75)
+    });
+});
+
+$linkContacto[1].addEventListener("click", ()=>{
+    window.scrollTo({
+        behavior: "smooth",
+        top: ($stnContacto.top-75)
     });
 });
 
